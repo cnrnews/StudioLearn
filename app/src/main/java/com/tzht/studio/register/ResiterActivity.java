6df8d411616e64fde2b134026de092144d919e06
candy <1601796593@qq.com>
2017-11-13 18:04:40 +0800

    Initial commit

---
+++ app/src/main/java/com/tzht/studio/register/ResiterActivity.java
@@ -0,0 +1,15 @@
+package com.tzht.studio.register;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.tzht.studio.R;
+
+public class ResiterActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
